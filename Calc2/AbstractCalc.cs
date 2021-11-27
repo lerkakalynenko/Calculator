46772dc49522e4ae36d407bd8e857b9534c8f192
Valeriya <valeria170503@gmail.com>
2021-11-27 15:36:56 +0200

    Completed task 2 uraa

--- Calc2/AbstractCalc.cs
+++ Calc2/AbstractCalc.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calc2
+﻿namespace Calc2
 {
     public abstract class  AbstractCalc
     {
-        public int Make { get; set; }
+        public string Make { get; set; }
         public abstract double Add();
         public abstract double Div();
         public abstract double Mul();
--- Calc2/OrdinaryCalc.cs
+++ Calc2/OrdinaryCalc.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Calc2
 {
-    public class OrdinaryCalc
+    public class OrdinaryCalc : AbstractCalc 
     {
         public OrdinaryCalc()
         {
@@ -15,22 +15,22 @@ namespace Calc2
         public double Number1 { get; set; }
         public double Number2 { get; set; }
 
-        public double Add()
+        public override double Add()
         {
             return Number1 + Number2;
         }
 
-        public double Sub()
+        public override double Sub()
         {
             return Number1 - Number2;
         }
 
-        public double Mul()
+        public override double Mul()
         {
             return Number1 * Number2;
         }
 
-        public double Div()
+        public override double Div()
         {
             return Number1 / Number2;
         }

181602bd7b24d7a837930460e9b357b602a9ca47
murzinaa <murzina.viktoria15@gmail.com>
2021-11-27 15:32:33 +0200

    Added abstract class

---
+++ Calc2/AbstractCalc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc2
+{
+    public abstract class  AbstractCalc
+    {
+        public int Make { get; set; }
+        public abstract double Add();
+        public abstract double Div();
+        public abstract double Mul();
+        public abstract double Sub();
+    }
+}

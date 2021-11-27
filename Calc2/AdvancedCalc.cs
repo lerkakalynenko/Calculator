faf6c365e2671f630fccf26843fe1cbe9e1275ce
murzinaa <murzina.viktoria15@gmail.com>
2021-11-27 15:21:16 +0200

    AdvancedCalc for task2

---
+++ Calc2/AdvancedCalc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc2
+{
+    public class AdvancedCalc : OrdinaryCalc, IAdvanced
+    {
+        public AdvancedCalc()
+        {
+
+        }
+        public double Cos()
+        {
+            double number = Number1 * Math.PI / 180;
+            return Math.Cos(number);
+        }
+    }
+}

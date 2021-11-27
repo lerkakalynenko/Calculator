9cf8786380ebd9cb9d5cfb905be444212301d1dc
Valeriya <valeria170503@gmail.com>
2021-11-27 13:42:29 +0200

    test1

--- Calculator/MainClass.cs
+++ Calculator/MainClass.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Console = System.Console;
 
 namespace Calculator
 {
     public class MainClass
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа через пробел");
+        }
         
     }
 }

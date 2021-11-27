f7d8d027804a48315e983e975ef42bae542a11ea
murzinaa <murzina.viktoria15@gmail.com>
2021-11-27 15:13:59 +0200

    added ordinaryCalc for task 2 try2

--- Calc2/OrdinaryCalc.cs
+++ Calc2/OrdinaryCalc.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Calc2
 {
-    class OrdinaryCalc
+    public class OrdinaryCalc
     {
+        public OrdinaryCalc()
+        {
+
+        }
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+
+        public double Add()
+        {
+            return Number1 + Number2;
+        }
+
+        public double Sub()
+        {
+            return Number1 - Number2;
+        }
+
+        public double Mul()
+        {
+            return Number1 * Number2;
+        }
+
+        public double Div()
+        {
+            return Number1 / Number2;
+        }
     }
 }

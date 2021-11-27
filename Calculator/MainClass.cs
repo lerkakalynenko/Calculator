fe57fbe2cd49e059a7dfecdeb876f3d44a4f6588
Valeriya <valeria170503@gmail.com>
2021-11-27 14:10:46 +0200

    menu

--- Calculator/MainClass.cs
+++ Calculator/MainClass.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+
 using Console = System.Console;
 
 namespace Calculator
@@ -12,8 +13,32 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите 2 числа через пробел");
+            
+            Console.WriteLine("Введите какой калькулятор вы хотите использовать, если обычный, то \n" +
+                              "введите 'o', если инженерный - 'a'");
+            string calc = Console.ReadLine();
+
+            string str = " ";
+            while (str != "exit")
+            {
+                Console.WriteLine("Введите 2 числа через пробел");
+                str = Console.ReadLine();
+                string[] array = str.Split(' ',(char) StringSplitOptions.RemoveEmptyEntries);
+                
+                int number1 = int.Parse(array[0]);
+                int number2 = int.Parse(array[1]);
+                if (calc == "o")
+                {
+                    Console.WriteLine("Введите операцию, которую вы хотите произвести над числами: \n" +
+                                      "+ => сложение; - => вычитание; * => умножение; / => деление"
+                    );
+                    
+                    
+                }
+            }
+
         }
+
         
     }
 }
